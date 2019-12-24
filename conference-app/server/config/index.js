22ad35d0a2244da5552366609793dc3d6b9b2fac
Mohammed Lazhari <lazhari.mohammed@outlook.com>
2019-12-24 20:17:17 +0100

    fix: Fixed the services version

--- conference-app/server/config/index.js
+++ conference-app/server/config/index.js
@@ -2,11 +2,11 @@ module.exports = {
   development: {
     sitename: 'Roux Meetups [Development]',
     serviceRegistryUrl: process.env.REGISTRY_HOST || 'http://localhost:3000',
-    serviceVersionIdentifier: '1.x.x',
+    serviceVersionIdentifier: '0.1.x',
   },
   production: {
     sitename: 'Roux Meetups',
     serviceRegistryUrl: process.env.REGISTRY_HOST || 'http://localhost:3000',
-    serviceVersionIdentifier: '1.x.x',
+    serviceVersionIdentifier: '0.1.x',
   },
 };
